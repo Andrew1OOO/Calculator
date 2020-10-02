e62b33f0190655226e1d701bd36e90e7825836d3
rayyanshaik2022 <56854894+rayyanshaik2022@users.noreply.github.com>
2020-10-02 11:38:20 -0400

    Equation class

--- src/src/Equation.java
+++ src/src/Equation.java
@@ -1,5 +1,20 @@
 public class Equation {
-    Equation(String equ) {
+    
+    private String equation;
+
+    public Equation(String equ) {
+        this.equation = equ;
+    }
+
+    public String clean(String s) {
+        s = s.replaceAll(" ", "");
+        if (s.charAt(0) == '(' && s.charAt(s.length()-1) == ')');
+            s = s.substring(1, s.length());
         
+        return s;
+    }
+
+    public void solve() {
+        System.out.println(this.equation);
     }
 }
